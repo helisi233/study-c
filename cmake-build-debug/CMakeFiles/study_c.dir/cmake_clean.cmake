a6ba470259650e47f460d73d240d6bc44a263104
helisi233 <hls12343@163.com>
2022-07-21 09:23:35 +0800

    slave

---
+++ cmake-build-debug/CMakeFiles/study_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study_c.dir/main.c.obj"
+  "study_c.pdb"
+  "study_c.exe"
+  "study_c.exe.manifest"
+  "libstudy_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/study_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
